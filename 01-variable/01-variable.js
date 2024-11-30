d1d0aaf8d2873ce0bd225c4245a49f44564f6c5c
singhakash123 <akashlife2004@gmail.com>
2024-11-30 13:09:37 +0530

    variable

--- 01-variable/01-variable.js
+++ 01-variable/01-variable.js
@@ -1,3 +1,24 @@
-// data type and variable 
 
-console.log('rohit singh chauhan');
+const accountId = 864863
+let accountEmail = "Akash@123"
+var accountPassword = '12345'
+accountCity = "Jaipur"
+let accountState ; 
+
+
+ // accountId = 2   // not allowed 
+
+ accountEmail = "Rohit@123"
+ accountPassword = '63636'
+ accountCity = 'Indore'
+   //accountState = 'mp'
+
+
+
+/* 
+prefer not to use var 
+because of an issue with block scope and functionl scope
+*/
+
+
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
