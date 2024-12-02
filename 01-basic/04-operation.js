91143b29e0073e202bc237f6365784ed3ef1aa98
singhakash123 <akashlife2004@gmail.com>
2024-12-02 18:18:45 +0530

    vriable_and-operator

--- 01-basic/04-operation.js
+++ 01-basic/04-operation.js
@@ -26,6 +26,7 @@ let negValue = -value
     console.log( "1" + 2 );
     console.log( "1" + 4 + 4);
     console.log( 1 + 4 + "4");
+
     console.log( "1" + (4 + 4));
     console.log((1 + 4) + "4");
     
@@ -39,6 +40,9 @@ let negValue = -value
    let num1 , num2 , num3 
    num1 = num2 = num3 = 2 + 2
 
+
+
+   
    let gameCounter = 144
      gameCounter++
      console.log(gameCounter);
