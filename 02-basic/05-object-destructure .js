b64191f8de3d7ee2a5511125de46650d607976fd
singhakash123 <akashlife2004@gmail.com>
2024-12-02 08:55:24 +0530

    json and destructure

---
+++ 02-basic/05-object-destructure .js
@@ -0,0 +1,44 @@
+
+
+const user ={
+    courseName : "Javascript in hindi",
+    courseDuration : "3 months",
+    courseFees : "5000"
+}
+
+const {courseDuration:duration} = user
+console.log(duration);
+
+// console.log(courseDuration);
+
+
+
+
+// destructure  in react 
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company ="appolo")
+
+
+// json formatter 
+// random user.me
+// https://api.github.com/users/hiteshchoudhary
+
+  // JSON
+  // {
+  //   "name" : "Akash Singh Chauhan",
+  //   "isLoogedIn" : true
+  //   "age" : 25 
+  // }
+
+  // [
+  //   {
+
+  //   },
+  //   {
+
+  //   }
+  // ]
