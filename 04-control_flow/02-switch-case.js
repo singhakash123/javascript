9f30b7344f88c2b7408b85e35b608f02ed2891df
singhakash123 <akashlife2004@gmail.com>
2024-12-03 09:39:54 +0530

    ontrol_flow

---
+++ 04-control_flow/02-switch-case.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//   case value:
+    
+//     break;
+
+//   default:
+//     break;
+// }
+
+const month = "March"
+switch (month) {
+  case "Jan":
+    console.log("this is jan")
+    break;
+  case "Feb":
+    console.log("this is Feb")
+    break;
+   case "March":
+      console.log("this is march")
+      break;
+
+  default:
+      console.log('no year natch here');  
+    break;
+}
---
+++ 04-control_flow/04-nullish_ternaray.js
@@ -0,0 +1,23 @@
+// nullish coalescing operator (??) : null , undefined 
+
+let value ; 
+   // value = 5 ?? 10
+   // value = null ?? 10 
+   // value = undefined ?? 15
+      value = null ?? 10 ?? 20
+
+    console.log(value);
+    
+
+// ternary operator (?,:)
+   // syntax 
+   // condition ? value_if_true : value_if_false
+
+let score = 200 
+  score > 100 ? console.log('your score is greater') : console.log('your score is less');
+  
+
+
+
+  // String: Length of characters.
+  // Array: Number of elements in the array.
